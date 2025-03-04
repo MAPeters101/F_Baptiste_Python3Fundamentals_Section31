90a6ca99b557c5bd20510bea0c43b45bf10e8772
Mark Peters <mapeters101@earthlink.net>
2025-03-03 22:17:37 -0500

    Descriptions for all exercises.

---
+++ exercises_all.py
@@ -0,0 +1,12 @@
+'''
+Question 1
+Alongside this notebook is a file daily_quotes.csv (the same one that we used for the Pandas exercises).
+
+Using this data write a function that generates a composite of line charts (2 columns wide), that charts the closing price for each symbol in the data.
+
+Question 2
+Expand on your previous function to include an n-day moving average on each chart. (When you calculate the moving average, be careful with the order of the data in each subset of data).
+
+Question 3
+Using the previous charts, add horizontal lines for the 25th, 50th and 75th percentiles of the close price (calculated over the entire time period).
+'''
