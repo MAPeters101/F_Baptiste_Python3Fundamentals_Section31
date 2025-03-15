64fdd5c5e8d0564843f9e8fb9abdb9a163381f66
Mark Peters <mapeters101@earthlink.net>
2025-03-14 23:33:54 -0400

    Descriptions for exercise01.

---
+++ exercise01.py
@@ -0,0 +1,8 @@
+'''
+Question 1
+Alongside this notebook is a file daily_quotes.csv (the same one that we used
+for the Pandas exercises).
+
+Using this data write a function that generates a composite of line charts
+(2 columns wide), that charts the closing price for each symbol in the data.
+'''
