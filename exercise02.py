a0732a2f480c0897075f167c66bc7dee1d29dfb2
Mark Peters <mapeters101@earthlink.net>
2025-03-14 23:34:26 -0400

    Descriptions for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,6 @@
+'''
+Question 2
+Expand on your previous function to include an n-day moving average on each
+chart. (When you calculate the moving average, be careful with the order of
+the data in each subset of data).
+'''
